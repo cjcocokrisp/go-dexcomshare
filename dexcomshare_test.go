10efe833e0f1de4c7e860195a9148dc2bfb1914c
cjcocokrisp <chriscoco1205@gmail.com>
2025-01-18 14:05:05 -0500

    feat: add 2 unit tests

---
+++ dexcomshare_test.go
@@ -0,0 +1,33 @@
+package dexcomshare
+
+import (
+	"testing"
+)
+
+func TestGetLatestEGV(t *testing.T) {
+	dexcom, err := Login("USERNAME", "PASSWORD")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = dexcom.GetLatestEGV()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetEGV(t *testing.T) {
+	dexcom, err := Login("USERNAME", "PASSWORD")
+	if err != nil {
+		t.Error(err)
+	}
+
+	egvs, err := dexcom.GetEGV(5, DefaultMinutes)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(egvs) != 5 {
+		t.Error(len(egvs))
+	}
+}
